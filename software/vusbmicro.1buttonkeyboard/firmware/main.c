b9cd98875f68f6a2b1e92854b452468a42896d58
overflo <f@gebenedeit.at>
2012-07-18 11:57:32 +0200

    Adding source code to use the vusbmicro as a general IO device

---
+++ software/vusbmicro.1buttonkeyboard/firmware/main.c
@@ -0,0 +1,293 @@
+/*
+
+
+Sample code for the vusbmicro device to read one button attached between GND and PIN B0 
+Will output "1" over USB keyboard as long as the button is pressed
+
+  ORIGINAL HEADER:
+
+ * Name: trustme.c
+ * Project: Thermostat based on AVR USB driver
+ * Author: Christian Starkjohann
+ * Creation Date: 2006-04-23
+ * Tabsize: 4
+ * Copyright: (c) 2006 by OBJECTIVE DEVELOPMENT Software GmbH
+ * License: Proprietary, free under certain conditions. See Documentation.
+ * This Revision: $Id: main.c 537 2008-02-28 21:13:01Z cs $
+ */
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include <stdlib.h>
+
+#include "usbdrv.h"
+#include "oddebug.h"
+
+
+
+#define BUTTON_PORT_B1 PORTB       /* PORTx - register for BUTTON 1 output */
+#define BUTTON_PIN_B1 PINB         /* PINx - register for BUTTON 1 input */
+#define BUTTON_BIT_B1 PB0          /* bit for BUTTON 1 input/output */
+
+
+static uchar    idleRate;           /* in 4 ms units */
+static uchar    newReport = 0;          /* current report */
+
+static uchar    buttonState_B1 = 3;             /*  stores state of button 0 */
+static uchar    buttonChanged_B1;
+
+static uchar    debounceTimeIsOver = 1; /* for switch debouncing */
+
+
+
+static uchar    reportBuffer[2];    /* buffer for HID reports */
+static uchar    idleRate;           /* in 4 ms units */
+static uchar    reportCount;		/* current report */
+
+/* ------------------------------------------------------------------------- */
+
+PROGMEM char usbHidReportDescriptor[USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH] = { /* USB report descriptor */
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x06,                    // USAGE (Keyboard)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
+    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
+    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x95, 0x08,                    //   REPORT_COUNT (8)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
+    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
+    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
+    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
+    0xc0                           // END_COLLECTION
+};
+/* We use a simplifed keyboard report descriptor which does not support the
+ * boot protocol. We don't allow setting status LEDs and we only allow one
+ * simultaneous key press (except modifiers). We can therefore use short
+ * 2 byte input reports.
+ * The report descriptor has been created with usb.org's "HID Descriptor Tool"
+ * which can be downloaded from http://www.usb.org/developers/hidpage/.
+ * Redundant entries (such as LOGICAL_MINIMUM and USAGE_PAGE) have been omitted
+ * for the second INPUT item.
+ */
+
+static void buildReport(void)
+{
+
+        uchar key;
+
+        if(newReport == 0){
+                if (buttonChanged_B1 == 1){
+                if (buttonState_B1 != 0){ // if button 1 is released
+                                key = 0; //button released event
+                        }
+                        else { //if button 1 is pressed
+                                key = 30; // key = '1'
+                }
+                        buttonChanged_B1 = 0;
+                        reportBuffer[1] = key;
+                }
+
+
+                newReport = 1;; //if no button has changed, the previous report will be sent
+        }
+
+
+
+}
+
+static void timerPoll(void)
+{
+	static unsigned int timerCnt;
+
+    if(TIFR & (1 << TOV1)){
+        TIFR = (1 << TOV1); /* clear overflow */
+        if(++timerCnt >= 5){       // 5/63 sec delay for switch debouncing
+                        timerCnt = 0;
+                        debounceTimeIsOver = 1;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+static void checkButtonChange(void) {
+
+        uchar tempButtonValue_B1 = bit_is_set(BUTTON_PIN_B1, BUTTON_BIT_B1); //status of switch is stored in tempButtonValue 
+        
+	if (tempButtonValue_B1 != buttonState_B1){ //if status has changed
+                buttonState_B1 = tempButtonValue_B1;    // change buttonState to new state
+                debounceTimeIsOver = 0; // debounce timer starts
+                newReport = 0; // initiate new report 
+                buttonChanged_B1 = 1;
+        }
+
+}
+
+
+
+
+
+
+
+/* ------------------------------------------------------------------------- */
+
+static void timerInit(void)
+{
+    TCCR1 = 0x0b;           /* select clock: 16.5M/1k -> overflow rate = 16.5M/256k = 62.94 Hz */
+}
+
+
+/* ------------------------------------------------------------------------- */
+/* ------------------------ interface to USB driver ------------------------ */
+/* ------------------------------------------------------------------------- */
+
+uchar	usbFunctionSetup(uchar data[8])
+{
+usbRequest_t    *rq = (void *)data;
+
+    usbMsgPtr = reportBuffer;
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
+        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+            /* we only have one report type, so don't look at wValue */
+            buildReport();
+            return sizeof(reportBuffer);
+        }else if(rq->bRequest == USBRQ_HID_GET_IDLE){
+            usbMsgPtr = &idleRate;
+            return 1;
+        }else if(rq->bRequest == USBRQ_HID_SET_IDLE){
+            idleRate = rq->wValue.bytes[1];
+        }
+    }else{
+        /* no vendor specific requests implemented */
+    }
+	return 0;
+}
+
+/* ------------------------------------------------------------------------- */
+/* ------------------------ Oscillator Calibration ------------------------- */
+/* ------------------------------------------------------------------------- */
+
+/* Calibrate the RC oscillator to 8.25 MHz. The core clock of 16.5 MHz is
+ * derived from the 66 MHz peripheral clock by dividing. Our timing reference
+ * is the Start Of Frame signal (a single SE0 bit) available immediately after
+ * a USB RESET. We first do a binary search for the OSCCAL value and then
+ * optimize this value with a neighboorhod search.
+ * This algorithm may also be used to calibrate the RC oscillator directly to
+ * 12 MHz (no PLL involved, can therefore be used on almost ALL AVRs), but this
+ * is wide outside the spec for the OSCCAL value and the required precision for
+ * the 12 MHz clock! Use the RC oscillator calibrated to 12 MHz for
+ * experimental purposes only!
+ */
+static void calibrateOscillator(void)
+{
+uchar       step = 128;
+uchar       trialValue = 0, optimumValue;
+int         x, optimumDev, targetValue = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
+
+    /* do a binary search: */
+    do{
+        OSCCAL = trialValue + step;
+        x = usbMeasureFrameLength();    /* proportional to current real frequency */
+        if(x < targetValue)             /* frequency still too low */
+            trialValue += step;
+        step >>= 1;
+    }while(step > 0);
+    /* We have a precision of +/- 1 for optimum OSCCAL here */
+    /* now do a neighborhood search for optimum value */
+    optimumValue = trialValue;
+    optimumDev = x; /* this is certainly far away from optimum */
+    for(OSCCAL = trialValue - 1; OSCCAL <= trialValue + 1; OSCCAL++){
+        x = usbMeasureFrameLength() - targetValue;
+        if(x < 0)
+            x = -x;
+        if(x < optimumDev){
+            optimumDev = x;
+            optimumValue = OSCCAL;
+        }
+    }
+    OSCCAL = optimumValue;
+}
+/*
+Note: This calibration algorithm may try OSCCAL values of up to 192 even if
+the optimum value is far below 192. It may therefore exceed the allowed clock
+frequency of the CPU in low voltage designs!
+You may replace this search algorithm with any other algorithm you like if
+you have additional constraints such as a maximum CPU clock.
+For version 5.x RC oscillators (those with a split range of 2x128 steps, e.g.
+ATTiny25, ATTiny45, ATTiny85), it may be useful to search for the optimum in
+both regions.
+*/
+
+void    usbEventResetReady(void)
+{
+    calibrateOscillator();
+    eeprom_write_byte(0, OSCCAL);   /* store the calibrated value in EEPROM */
+}
+
+/* ------------------------------------------------------------------------- */
+/* --------------------------------- main ---------------------------------- */
+/* ------------------------------------------------------------------------- */
+
+int main(void)
+{
+uchar   i;
+uchar   calibrationValue;
+
+    calibrationValue = eeprom_read_byte(0); /* calibration value from last time */
+    if(calibrationValue != 0xff){
+        OSCCAL = calibrationValue;
+    }
+    odDebugInit();
+    usbDeviceDisconnect();
+    for(i=0;i<20;i++){  /* 300 ms disconnect */
+        _delay_ms(15);
+    }
+    usbDeviceConnect();
+
+    wdt_enable(WDTO_1S);
+
+
+        /* turn on internal pull-up resistor for the switches */
+    BUTTON_PORT_B1 |= _BV(BUTTON_BIT_B1);
+
+
+    timerInit();
+
+    usbInit();
+    sei();
+    for(;;){    /* main event loop */
+        wdt_reset();
+        usbPoll();
+
+
+        if (debounceTimeIsOver == 1){
+                    checkButtonChange();
+        }
+
+
+        if(usbInterruptIsReady() && newReport == 0){ /* we can send another report */
+
+            buildReport();
+            usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
+        	}
+        timerPoll();
+    }
+    return 0;
+}
